494d6240315eb2007c4793deeb6f5cecf9b6f2df
Demid Rostovtsev <demid.rostovtsev@gmail.com>
2022-01-18 21:18:27 +0400

    Initial project

---
+++ src/main/java/com/github/demideinherjar/webexampleproject/WebExampleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.demideinherjar.webexampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebExampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebExampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/demideinherjar/webexampleproject/WebExampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.demideinherjar.webexampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebExampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
